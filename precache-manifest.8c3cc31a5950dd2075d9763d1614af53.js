4142c6b5d7518c13f1cd942220aa96b48090e8ad
Tudor Suruceanu <tudor.suruceanu@yahoo.com>
2020-05-09 20:30:05 +0100

    Updates

---
+++ precache-manifest.8c3cc31a5950dd2075d9763d1614af53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1854a0e7dc65ce28d497c99b1f7082c0",
+    "url": "/not-geoguessr/index.html"
+  },
+  {
+    "revision": "46d77a722d32332f0240",
+    "url": "/not-geoguessr/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff5ee0603dc427034c35",
+    "url": "/not-geoguessr/static/js/2.2f149294.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/not-geoguessr/static/js/2.2f149294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d77a722d32332f0240",
+    "url": "/not-geoguessr/static/js/main.ea28db17.chunk.js"
+  },
+  {
+    "revision": "f832c1f3f5b071356ea9",
+    "url": "/not-geoguessr/static/js/runtime-main.95888916.js"
+  }
+]);
